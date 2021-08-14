c7489acba207966e67402e97646db186c26a3330
polware <76798994+polware@users.noreply.github.com>
2021-08-14 17:06:00 -0500

    Add files via upload
    Archivos fuente del Reto # 5.

---
+++ Reto5/src/main/java/com/mycompany/reto5/App.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.reto5;
+import Vista.VentanaPrincipal;
+
+/**
+ *
+ * @author Zero_
+ */
+public class App {
+    public static void main(String args[]){        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new VentanaPrincipal().setVisible(true);
+            }
+        });
+    }
+}
